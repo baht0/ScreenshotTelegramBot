6b022a24b67d4d45e0a43520d1cd856aa856ce13
Rasim <bahtiarov.rasim@gmail.com>
2023-09-23 15:40:18 +0300

    Add project files.

---
+++ ScreenshotTelegramBot/MainForm.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace ScreenshotTelegramBot
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.startButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timerNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.tokenTextBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.captionСomboBox = new System.Windows.Forms.ComboBox();
+            this.chatTextBox = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.timerLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.timerNumericUpDown)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.startButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startButton.Location = new System.Drawing.Point(6, 147);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(256, 33);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "START";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(2, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Caption:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(17, 114);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 24);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Timer:";
+            // 
+            // timerNumericUpDown
+            // 
+            this.timerNumericUpDown.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timerNumericUpDown.Location = new System.Drawing.Point(87, 112);
+            this.timerNumericUpDown.Maximum = new decimal(new int[] {
+            360,
+            0,
+            0,
+            0});
+            this.timerNumericUpDown.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.timerNumericUpDown.Name = "timerNumericUpDown";
+            this.timerNumericUpDown.Size = new System.Drawing.Size(120, 29);
+            this.timerNumericUpDown.TabIndex = 3;
+            this.timerNumericUpDown.Value = new decimal(new int[] {
+            300,
+            0,
+            0,
+            0});
+            // 
+            // timer
+            // 
+            this.timer.Interval = 1000;
+            this.timer.Tick += new System.EventHandler(this.Timer_Tick);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(213, 114);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 24);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "sec.";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.tokenTextBox);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.captionСomboBox);
+            this.groupBox1.Controls.Add(this.chatTextBox);
+            this.groupBox1.Controls.Add(this.startButton);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.timerNumericUpDown);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 25);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(268, 190);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            // 
+            // tokenTextBox
+            // 
+            this.tokenTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tokenTextBox.Location = new System.Drawing.Point(87, 82);
+            this.tokenTextBox.Name = "tokenTextBox";
+            this.tokenTextBox.Size = new System.Drawing.Size(171, 24);
+            this.tokenTextBox.TabIndex = 19;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(12, 81);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(69, 24);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "Token:";
+            // 
+            // captionСomboBox
+            // 
+            this.captionСomboBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.captionСomboBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.captionСomboBox.FormattingEnabled = true;
+            this.captionСomboBox.Items.AddRange(new object[] {
+            "ТП1",
+            "ТП1 ЮЛ",
+            "ТП2"});
+            this.captionСomboBox.Location = new System.Drawing.Point(86, 20);
+            this.captionСomboBox.Name = "captionСomboBox";
+            this.captionСomboBox.Size = new System.Drawing.Size(172, 26);
+            this.captionСomboBox.TabIndex = 6;
+            this.captionСomboBox.Text = "ТП1";
+            // 
+            // chatTextBox
+            // 
+            this.chatTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.chatTextBox.Location = new System.Drawing.Point(86, 52);
+            this.chatTextBox.Name = "chatTextBox";
+            this.chatTextBox.Size = new System.Drawing.Size(172, 24);
+            this.chatTextBox.TabIndex = 17;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(8, 51);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(73, 24);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Chat Id:";
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.Location = new System.Drawing.Point(12, 218);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(40, 13);
+            this.statusLabel.TabIndex = 11;
+            this.statusLabel.Text = "Status:";
+            this.toolTip1.SetToolTip(this.statusLabel, "HTTP Telegram API");
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.LightGray;
+            this.label4.Location = new System.Drawing.Point(152, 218);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(128, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Rasim Bakhtiyarov - 2023";
+            // 
+            // timerLabel
+            // 
+            this.timerLabel.AutoSize = true;
+            this.timerLabel.Location = new System.Drawing.Point(12, 9);
+            this.timerLabel.Name = "timerLabel";
+            this.timerLabel.Size = new System.Drawing.Size(97, 13);
+            this.timerLabel.TabIndex = 20;
+            this.timerLabel.Text = "Next check: 0 sec.";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 239);
+            this.Controls.Add(this.timerLabel);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.statusLabel);
+            this.Controls.Add(this.label4);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Screenshot Telegram Bot";
+            ((System.ComponentModel.ISupportInitialize)(this.timerNumericUpDown)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown timerNumericUpDown;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ComboBox captionСomboBox;
+        private System.Windows.Forms.Label statusLabel;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tokenTextBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox chatTextBox;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label timerLabel;
+    }
+}
+
---
+++ ScreenshotTelegramBot/MainForm.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScreenshotTelegramBot
+{
+    public partial class MainForm : Form
+    {
+        private decimal TimerSec = 0;
+        private string ScreenFileName => AppDomain.CurrentDomain.BaseDirectory + "screen.png";
+
+        public MainForm() => InitializeComponent();
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(captionСomboBox.Text))
+            {
+                groupBox1.Enabled = false;
+                TimerSec = timerNumericUpDown.Value;
+                timer.Enabled = true;
+            }
+        }
+        private async void Timer_Tick(object sender, EventArgs e)
+        {
+            timerLabel.Text = $"Next check: {TimerSec} sec.";
+            if (TimerSec <= 0)
+            {
+                TimerSec = timerNumericUpDown.Value;
+                TakeScreen();
+                await Send();
+            }
+            TimerSec--;
+        }
+
+        //Screenshot
+        private void TakeScreen()
+        {
+            try
+            {
+                int screenLeft = SystemInformation.VirtualScreen.Left;
+                int screenTop = SystemInformation.VirtualScreen.Top;
+                int screenWidth = SystemInformation.VirtualScreen.Width;
+                int screenHeight = SystemInformation.VirtualScreen.Height;
+
+                var bitmap = new Bitmap(screenWidth, screenHeight);
+                var g = Graphics.FromImage(bitmap);
+                g.CopyFromScreen(screenLeft, screenTop, 0, 0, bitmap.Size);
+                bitmap.Save(ScreenFileName, ImageFormat.Png);
+            }
+            catch (Exception ex)
+            {
+                statusLabel.Text = "Status: Exception (take screen)";
+                File.WriteAllText(AppDomain.CurrentDomain.BaseDirectory + "ex_take_screen.txt", ex.Message);
+            }
+        }
+        //Telegram API
+        private async Task Send()
+        {
+            try
+            {
+                string chat_id = chatTextBox.Text;
+                string bot_token = tokenTextBox.Text;
+
+                var fileStream = new FileStream(ScreenFileName, FileMode.Open, FileAccess.Read);
+                var content = new MultipartFormDataContent
+                {
+                    { new StringContent(chat_id, Encoding.UTF8), "chat_id" },
+                    { new StreamContent(fileStream), "photo", fileStream.Name },
+                    { new StringContent(captionСomboBox.Text, Encoding.UTF8), "caption" }
+                };
+
+                var client = new HttpClient();
+                var res = await client.PostAsync("https://api.telegram.org/bot" + bot_token + "/sendPhoto?", content);
+                statusLabel.Text = "Status: " + res.StatusCode.ToString();
+            }
+            catch (Exception ex)
+            {
+                statusLabel.Text = "Status: Exception (send)";
+                File.WriteAllText(AppDomain.CurrentDomain.BaseDirectory + "ex_send.txt", ex.Message);
+            }
+        }
+    }
+}
